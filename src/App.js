ab99927dee1a1f092ad533bced4e6dd80d5742dd
TClaypool00 <claypooltyler00@gmail.com>
2023-08-31 20:30:04 -0500

    Router implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './components/Pages/Home';
+import Register from './components/Pages/Account/Register';
+import Login from './components/Pages/Account/Login';
+import Navbar from "./components/NavBar/Navbar";
+
+
 function App() {
   return (
-    <div>
-      <p>Hello world!</p>
-    </div>
+    <>
+      <Navbar />
+        <BrowserRouter>
+          <Routes>
+          <Route index element={<Home />} />
+          <Route path="account/login" element={<Login />} />
+          <Route path="account/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
