0a7526ef5dd7dcbe92f75bfe74f8d18d6ec76611
TClaypool00 <claypooltyler00@gmail.com>
2023-09-05 20:49:13 -0500

    Created custom  Errors model instead of generic object type

---
+++ src/Models/Errrors.ts
@@ -0,0 +1,11 @@
+export class Errors {
+    //#region  constructors
+    constructor () {
+        this.hasError = false;
+    }
+    //#endregion
+
+    //#region  public memembers
+    public hasError : boolean;
+    //#endregion
+}
